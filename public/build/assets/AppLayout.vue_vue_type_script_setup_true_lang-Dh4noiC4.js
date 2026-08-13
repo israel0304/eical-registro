16fac00ad957311c6f63f29de485a83f354d1b84
Israel Hernández <israel0304@gmail.com>
2026-08-12 22:27:04 -0600

    chore: compilar assets de produccion

--- public/build/assets/AppLayout.vue_vue_type_script_setup_true_lang-Dh4noiC4.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./AppSidebarLayout.vue_vue_type_script_setup_true_lang-CIAGk5S2.js";import{d as s,c as t,w as o,U as c,o as m}from"./app-BzkBrS9_.js";const p=s({__name:"AppLayout",props:{breadcrumbs:{default:()=>[]}},setup(e){return(r,n)=>(m(),t(a,{breadcrumbs:e.breadcrumbs},{default:o(()=>[c(r.$slots,"default")]),_:3},8,["breadcrumbs"]))}});export{p as _};
