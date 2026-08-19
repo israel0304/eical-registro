0281fccffe66f076798904aa32959315a99ea185
Israel Hernández <israel0304@gmail.com>
2026-08-18 18:43:15 -0600

    fix: Mis Ponencias se muestra cuando presentations.my esta activo sin importar presentations.view

---
+++ public/build/assets/AppLayout.vue_vue_type_script_setup_true_lang-S0uX741r.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./AppSidebarLayout.vue_vue_type_script_setup_true_lang-BsiJBb7n.js";import{d as s,c as t,w as o,U as c,o as m}from"./app-C9YD3EdK.js";const p=s({__name:"AppLayout",props:{breadcrumbs:{default:()=>[]}},setup(e){return(r,n)=>(m(),t(a,{breadcrumbs:e.breadcrumbs},{default:o(()=>[c(r.$slots,"default")]),_:3},8,["breadcrumbs"]))}});export{p as _};
