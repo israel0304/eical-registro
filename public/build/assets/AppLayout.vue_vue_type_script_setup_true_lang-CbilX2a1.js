2f53d67d475170ca2d4829311c5aff4f940e86b2
Israel Hernández <israel0304@gmail.com>
2026-08-24 20:20:55 -0600

    security: exclude build assets from git, fix 20 npm vulnerabilities (vite)

--- public/build/assets/AppLayout.vue_vue_type_script_setup_true_lang-CbilX2a1.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./AppSidebarLayout.vue_vue_type_script_setup_true_lang-DUPG3M4C.js";import{d as s,c as t,w as o,U as c,o as m}from"./app-CY4pX9JX.js";const p=s({__name:"AppLayout",props:{breadcrumbs:{default:()=>[]}},setup(e){return(r,n)=>(m(),t(a,{breadcrumbs:e.breadcrumbs},{default:o(()=>[c(r.$slots,"default")]),_:3},8,["breadcrumbs"]))}});export{p as _};
